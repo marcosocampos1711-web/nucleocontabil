895c160e98a01d4b2777fae88063786255e7df8b
marcosocampos1711-web <marcosocampos1711@gmail.com>
2026-01-05 15:29:31 -0300

    Delete nucleo.js

--- nucleo.js
+++
@@ -1,9 +0,0 @@
-// Efeito do Menu ao Rolar
-window.addEventListener("scroll", function() {
-	const nav = document.getElementById("main-nav");
-	if (window.scrollY > 50) {
-		nav.classList.add("sticky");
-	} else {
-		nav.classList.remove("sticky");
-	}
-});
