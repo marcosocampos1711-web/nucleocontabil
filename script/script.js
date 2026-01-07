262904678502c9192ae5d6942bd06f99506d6593
marcosocampos1711-web <marcosocampos1711@gmail.com>
2026-01-06 22:08:01 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-
+// Efeito do Menu ao Rolar
+window.addEventListener("scroll", function() {
+	const nav = document.getElementById("main-nav");
+	if (window.scrollY > 50) {
+		nav.classList.add("sticky");
+	} else {
+		nav.classList.remove("sticky");
+	}
+});
